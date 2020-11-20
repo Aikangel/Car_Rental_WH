5cbad933382960be337091d6b166aa7fe60f9f72
Aikangel <mr.sda2014@yandex.ru>
2020-11-20 12:31:03 +0300

    INIT

---
+++ Car_Rental_WH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Car_Rental_WH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
